8473c9a610ef1a44aaf71cb40dacb76bf39133b7
AmodioR <Patricktobiassteppat@gmail.com>
2026-02-04 20:52:37 +0100

    Restore main tab shell navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'screens/account_screen.dart';
-import 'screens/home_screen.dart';
 import 'screens/landing_screen.dart';
-import 'screens/open_chats_screen.dart';
+import 'screens/main_shell.dart';
 import 'theme/app_colors.dart';
 
 
@@ -35,54 +33,6 @@ class BlindDateApp extends StatelessWidget {
 ),
 
       home: isLoggedIn ? const MainShell() : const LandingScreen(),
-    );
-  }
-}
-
-class MainShell extends StatefulWidget {
-  const MainShell({super.key});
-
-  @override
-  State<MainShell> createState() => _MainShellState();
-}
-
-class _MainShellState extends State<MainShell> {
-  int _index = 0;
-
-  final _pages = const [
-    HomeScreen(),
-    OpenChatsScreen(),
-    AccountScreen(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_index],
-      bottomNavigationBar: SizedBox(
-        height: kBottomNavigationBarHeight + 16,
-        child: BottomNavigationBar(
-          currentIndex: _index,
-          onTap: (i) => setState(() => _index = i),
-          selectedItemColor: AppColors.primary,
-          unselectedItemColor: Colors.black45,
-          showUnselectedLabels: true,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home_outlined),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.chat_bubble_outline),
-              label: 'Chats',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person_outline),
-              label: 'Konto',
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
