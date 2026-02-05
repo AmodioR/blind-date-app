9ca61e989d33bd351227766a1df51216aa146013
AmodioR <Patricktobiassteppat@gmail.com>
2026-02-05 16:09:28 +0100

    Checkpoint: mutual matchmaking + realtime chat + profiles

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -2,6 +2,6 @@ class AppConfig {
   const AppConfig._();
 
   static const bool useRemoteProfile = true;
-  static const bool useRemoteChat = false;
+  static const bool useRemoteChat = true;
   static const bool useRemoteMatchmaking = true;
 }
