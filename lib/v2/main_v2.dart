8d2da0481534301b2954c4250e504d0ed1fa9a26
AmodioR <Patricktobiassteppat@gmail.com>
2026-08-13 15:44:08 +0200

    Start BlindDate 2.0 with onboarding flow

--- lib/v2/main_v2.dart
+++ lib/v2/main_v2.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'interactive_app.dart';
+import 'flow_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const BlindDateV2InteractiveApp());
+  runApp(const BlindDateV2FlowApp());
 }
