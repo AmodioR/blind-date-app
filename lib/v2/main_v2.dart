7742917fdfe858f43ffd05037d3db3486c391021
AmodioR <Patricktobiassteppat@gmail.com>
2026-08-13 15:33:24 +0200

    Add BlindDate 2.0 preview entrypoint

---
+++ lib/v2/main_v2.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'interactive_app.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const BlindDateV2InteractiveApp());
+}
